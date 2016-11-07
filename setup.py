3def881d260bdd15979508761219d2cc865a6312
Michael Wayne Goodman <goodman.m.w@gmail.com>
2016-11-07 15:25:41 -0800

    Release v0.1.0

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup
 
 long_description = (
-    'The Penman library and utility allow for serializing graphs in the'
-    'PENMAN format (e.g., as used by AMR (Abstract Meaning Representation)).'
-    'This utility not only converts from PENMAN to triples, but also can'
+    'The Penman library and utility allow for serializing graphs in the '
+    'PENMAN format (e.g., as used by AMR (Abstract Meaning Representation)). '
+    'This utility not only converts from PENMAN to triples, but also can '
     'reserialize PENMAN to re-indent or select a new top node.'
 )
 
