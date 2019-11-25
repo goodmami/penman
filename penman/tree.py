837cafbc65403b43defd1d623a8c8e016d8ac83f
Michael Wayne Goodman <goodman.m.w@gmail.com>
2019-11-25 12:20:48 +0800

    Add __str__() to Graph and Tree
    For diagnostic purposes, these print with indentation, unlike
    __repr__().

--- penman/tree.py
+++ penman/tree.py
@@ -38,11 +38,31 @@ class Tree:
     def __repr__(self) -> str:
         return 'Tree({!r})'.format(self.node)
 
+    def __str__(self) -> str:
+        return 'Tree(\n  {})'.format(_format(self.node, 2))
+
     def nodes(self):
         """
         Return the nodes in the tree as a flat list.
         """
         return _nodes(self.node)
+
+
+def _format(node, level):
+    var, edges = node
+    next_level = level + 2
+    indent = '\n' + ' ' * next_level
+    edges = [_format_edge(edge, next_level) for edge in edges]
+    return '({!r}, [{}{}])'.format(var, indent, (',' + indent).join(edges))
+
+
+def _format_edge(edge, level):
+    role, target, epidata = edge
+    if is_atomic(target):
+        target = repr(target)
+    else:
+        target = _format(target, level)
+    return '({!r}, {}, {!r})'.format(role, target, epidata)
 
 
 def _nodes(node):
